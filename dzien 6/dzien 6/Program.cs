edb5b36279f66307cceffe166579a04377f37b8a
Karol_m1 <125696703+Karolm1@users.noreply.github.com>
2023-11-06 23:26:51 +0100

    dzien 6

---
+++ dzien 6/dzien 6/Program.cs
@@ -0,0 +1,48 @@
+﻿Employee employee1 = new Employee("Adam", "Nowak", 32);
+Employee employee2 = new Employee("Weronika", "Górska", 29);
+Employee employee3 = new Employee("Dominik", "Weszyński", 38);
+
+employee1.AddScore(6);
+employee1.AddScore(8);
+employee1.AddScore(4);
+employee1.AddScore(2);
+employee1.AddScore(4);
+
+employee2.AddScore(4);
+employee2.AddScore(6);
+employee2.AddScore(7);
+employee2.AddScore(5);
+employee2.AddScore(3);
+
+employee3.AddScore(1);
+employee3.AddScore(4);
+employee3.AddScore(6);
+employee3.AddScore(3);
+employee3.AddScore(5);
+
+List<Employee> employees = new List<Employee>()
+{
+  employee1, employee2, employee3
+};
+
+int maxResult = -1;
+
+Employee employeeWithMaxResult = null;
+
+foreach (var employee in employees)
+{
+    if (employee.Result > maxResult)
+    {
+        employeeWithMaxResult = employee;
+        maxResult = employeeWithMaxResult.Result;
+    }
+}
+
+var name = employeeWithMaxResult.name;
+var lastname = employeeWithMaxResult.lastname;
+var age = employeeWithMaxResult.age;
+var result = employeeWithMaxResult.Result;
+
+Console.WriteLine
+
+("Pracownik z największą liczbą punktów:" + name + " " + lastname + ", " + age + "lat," + result + " punktów.");       
---
+++ dzien 6/dzien 6/User.cs
@@ -0,0 +1,26 @@
+﻿public class Employee
+{
+    private List<int> score = new List<int>();
+
+    public Employee(string name, string lastname, int age)
+    {
+        this.name = name;
+        this.lastname = lastname;
+        this.age = age;
+    }
+
+    public string name { get; private set; }
+    public string lastname { get; private set; }
+    public int age { get; private set; }
+    public int Result
+    {
+        get
+        {
+            return this.score.Sum();
+        }
+    }
+    public void AddScore(int number)
+    {
+        this.score.Add(number);
+    }
+}
